d25e6cf5434983c57fc867df9a4779e42a4b05e2
nitin <nitinkumarchauhan0807@gmail.com>
2022-02-28 22:08:29 +0530

    my change

--- freefire/urls.py
+++ freefire/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("giveaway/",include("giveaway.urls"),name="giveaway"),
     path("",include("home.urls"),name="home"),
-    path("auth/",include("authantic.urls"),name="auth")
+    path("auth/",include("authantic.urls"),name="auth"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
