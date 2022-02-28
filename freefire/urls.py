06f22e4a080284cb0571701ea200ad8728aa6134
nitin <nitinkumarchauhan0807@gmail.com>
2022-02-28 22:50:28 +0530

    my change

--- freefire/urls.py
+++ freefire/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import url
 from django.views.static import serve
 
 urlpatterns = [
@@ -25,8 +24,8 @@ urlpatterns = [
     path("giveaway/",include("giveaway.urls"),name="giveaway"),
     path("",include("home.urls"),name="home"),
     path("auth/",include("authantic.urls"),name="auth"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
